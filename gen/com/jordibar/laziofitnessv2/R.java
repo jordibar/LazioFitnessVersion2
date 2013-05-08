b13a9616d816654d0ce10f555a5519fd5ecc2889
Jordi Barrero <jordibar@gmail.com>
2013-05-08 21:15:42 +0200

    Se ha cambiado el menu TabHost por uno mas personalizable

--- gen/com/jordibar/laziofitnessv2/R.java
+++ gen/com/jordibar/laziofitnessv2/R.java
@@ -10,6 +10,9 @@ package com.jordibar.laziofitnessv2;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int naranja=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,15 +20,22 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int tab_bg_selected=0x7f020001;
+        public static final int tab_bg_selector=0x7f020002;
+        public static final int tab_bg_unselected=0x7f020003;
+        public static final int tab_divider=0x7f020004;
+        public static final int tab_text_selector=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f090003;
+        public static final int tabsLayout=0x7f090001;
+        public static final int tabsText=0x7f090002;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,14 +43,15 @@ public final class R {
         public static final int horarios=0x7f030002;
         public static final int noticias=0x7f030003;
         public static final int principal=0x7f030004;
+        public static final int tabs_bg=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -64,16 +75,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        /**  EstiloTabBar 
-         */
-        public static final int EstiloTabBar=0x7f060003;
+        public static final int AppTheme=0x7f070001;
         /**  Para no mostrar la barra del nombre de la aplicacion 
          */
-        public static final int NoBarraTitulo=0x7f060002;
+        public static final int NoBarraTitulo=0x7f070002;
     }
 }
--- src/com/jordibar/laziofitnessv2/MainActivity.java
+++ src/com/jordibar/laziofitnessv2/MainActivity.java
@@ -1,57 +1,89 @@
 package com.jordibar.laziofitnessv2;
 
+
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Color;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TabHost;
-import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 public class MainActivity extends TabActivity{
 
-	TabHost tabHost;
+	private TabHost mTabHost;
+	
+	private void setupTabHost() {
+		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+		mTabHost.setup();
+	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//------------------MENU-----------------------------------------------------------------------------------
-		tabHost = getTabHost(); //Creamos el tabHost de la actividad
-		TabHost.TabSpec spec; //Creamos un recurso para las propiedades de la pestana
-		Intent intent; //Intent que se uilitzara para abrir cada pestana
-		Resources res = getResources(); //Obtenemos los recursos
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if(!prefs.getBoolean("firstTime", false))
+		{
+			//La primera vez que se ejecuta
+			SharedPreferences.Editor editor =prefs.edit();
+			editor.putBoolean("firstTime", true);
+			editor.commit();
+		}
+				
+		setupTabHost();
+		mTabHost.getTabWidget().setDividerDrawable(R.drawable.tab_divider);
 		
+		//Añadimos las pestanas
+		setupTab(new TextView(this), "Principal", "Principal.class");
+		setupTab(new TextView(this), "Contacto", "Contacto.class");
+		setupTab(new TextView(this), "Horarios Clases", "Horarios.class");
+		setupTab(new TextView(this), "Noticias", "Noticias.class");
+		}
 		
+	
+	
+		private void setupTab(final View view, final String tag, final String className)
+		{
+			View tabview = createTabView(mTabHost.getContext(), tag);
+			TabHost tabHost = getTabHost();
+			TabHost.TabSpec spec;
+			Intent intent;
+			intent = new Intent().setClass(this, Principal.class);
+			if (className.equals("Principal.class"))
+			{
+				intent = new Intent().setClass(this, Principal.class);
+			}
+			if (className.equals("Contacto.class"))
+			{
+				intent = new Intent().setClass(this, Contacto.class);
+			}
+			if (className.equals("Horarios.class"))
+			{
+				intent = new Intent().setClass(this, Horarios.class);
+			}
+			if (className.equals("Noticias.class"))
+			{
+				intent = new Intent().setClass(this, Noticias.class);
+			}
+			
+			TabSpec setContent = mTabHost.newTabSpec(tag).setIndicator(tabview).setContent(intent);
+			
+			mTabHost.addTab(setContent);
+		}
+	
 		
-		//Se crea el intent para abrir la actividad que en realidad es una clase
-		intent = new Intent().setClass(this,  Principal.class);
-		//Se configura la pesrana con sus propiedades
-		spec = tabHost.newTabSpec("Principal").setIndicator("Principal").setContent(intent);
-		//Se carga la pestana en el contenedor TabHost
-		tabHost.addTab(spec);
-		
-		
-		
-		//Repetimos lo mismo por cada pestana
-		intent = new Intent().setClass(this,  Contacto.class);
-		spec = tabHost.newTabSpec("Contacto").setIndicator("Contacto").setContent(intent);
-		tabHost.addTab(spec);
-		
-		intent = new Intent().setClass(this,  Horarios.class);
-		spec = tabHost.newTabSpec("Horarios").setIndicator("Horarios Classes").setContent(intent);
-		tabHost.addTab(spec);
-		
-		intent = new Intent().setClass(this,  Noticias.class);
-		spec = tabHost.newTabSpec("Noticias").setIndicator("Noticias").setContent(intent);
-		tabHost.addTab(spec);
-		
-
-    
-		//-----------------\MENU-----------------------------------------------------------------------------------
-	}
-
-
-
+		private static View createTabView(final Context context, final String text)
+		{
+			View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
+			TextView tv = (TextView) view.findViewById(R.id.tabsText);
+			tv.setText(text);
+			return view;
+		}
 }
+
